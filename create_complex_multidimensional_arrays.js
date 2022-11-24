9e23d4b799b8fd583a0f7444716955ba114f5257
jeffbai996 <j.bai@wustl.edu>
2022-11-23 22:11:34 -0800

    Added completed code for Create Complex Multi-Dimensional Arrays

--- create_complex_multidimensional_arrays.js
+++ create_complex_multidimensional_arrays.js
@@ -33,10 +33,20 @@
 
 let myNestedArray = [
   // Only change code below this line
-  ['unshift', false, 1, 2, 3, 'complex', 'nested'],
-  ['loop', 'shift', 6, 7, 1000, 'method'],
-  ['concat', false, true, 'spread', 'array'],
-  ['mutate', 1327.98, 'splice', 'slice', 'push'],
-  ['iterate', 1.3849, 7, '8.4876', 'arbitrary', 'depth']
+  ['unshift', false],
+  [
+    [1, 2, 3, 'deep', 'nested']
+  ],
+  [
+    [
+      ['loop', 'deeper', 6, 7, 1000, 'method']
+    ],
+    [
+      [
+        ['deepest']
+      ]
+
+      ]
+    ]
   // Only change code above this line
 ];
