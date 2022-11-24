6c5662e5fb5ecac6efcff0b5a4f5fe13687f081b
jeffbai996 <j.bai@wustl.edu>
2022-11-23 22:15:41 -0800

    Added completed code for Add Key-Value Pairs to JavaScript Objects

--- add_key_value_pairs_to_js_objects.js
+++ add_key_value_pairs_to_js_objects.js
@@ -35,7 +35,9 @@ let foods = {
 };
 
 // Only change code below this line
-
+foods.bananas = 13;
+foods.grapes = 35;
+foods.strawberries = 27;
 // Only change code above this line
 
 console.log(foods);
