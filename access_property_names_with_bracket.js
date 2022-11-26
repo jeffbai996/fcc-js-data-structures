02c61748f109bc92b0ecbb3ed3f462fa9352a347
jeffbai996 <j.bai@wustl.edu>
2022-11-25 16:59:57 -0800

    Added completed code for Access Property Names with Bracket Notation

--- access_property_names_with_bracket.js
+++ access_property_names_with_bracket.js
@@ -18,7 +18,8 @@ let foods = {
 
 function checkInventory(scannedItem) {
   // Only change code below this line
-
+  let result = foods[scannedItem];
+  return result;
   // Only change code above this line
 }
 
