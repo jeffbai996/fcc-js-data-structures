f70e36c85cb014ab5e353f229095463c42cd146f
jeffbai996 <j.bai@wustl.edu>
2022-11-28 10:31:52 -0800

    Added completed code for Iterate Through the Keys of an Object with a for .. in statement

--- iterate_through_keys_with_forin.js
+++ iterate_through_keys_with_forin.js
@@ -38,7 +38,13 @@ const users = {
 
 function countOnline(usersObj) {
   // Only change code below this line
-
+  let onlineUsers = 0;
+  for (let user in usersObj) {
+    if (usersObj[user].online === true) {
+      onlineUsers++;
+    }
+  }
+  return onlineUsers;
   // Only change code above this line
 }
 
